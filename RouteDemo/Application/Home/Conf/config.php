afc210e9a290c8488595fa0c62a6df4d8d23d91b
Marc <zunly@hotmail.com>
2018-12-16 11:30:47 +0800

    在routedemo下面实现简单的路由, URL_ROUTER_ON, 不是ROUTE_ON

--- RouteDemo/Application/Home/Conf/config.php
+++ RouteDemo/Application/Home/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //'配置项'=>'配置值'
+    'URL_ROUTE_RULES' => array(
+        'posts/:year/:month/:day' => 'Index/index',
+        'posts/:id' => 'Index/index',
+        'posts/read/:id' => '/posts/:1'),
 );
