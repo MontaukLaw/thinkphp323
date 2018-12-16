1b4fbc6ef7e456b6f5405f7b98631b4e50be397e
Marc <zunly@hotmail.com>
2018-12-16 12:18:30 +0800

    增加了controllerdemo模块, 1. 要增加.htaccess文件, 这样才能直接访问模块, 不然就要加入口文件名. 2. controller文件名后缀要加上.class.php, 不然说发现不了contoller类, 这些问题在5.0里面应该都不存在,不过按照规则来, 问题也不大

---
+++ ControllerDemo/Application/Home/Controller/AnotherController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zunly
+ * Date: 2018/12/16
+ * Time: 12:05
+ */
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class AnotherController extends Controller
+{
+    public function test()
+    {
+        echo 'If you can see this, you are the hero!';
+    }
+}
