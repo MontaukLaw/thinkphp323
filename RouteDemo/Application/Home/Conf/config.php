b20eea7f545ad12ec3bd9afea015ba591e032899
Marc <zunly@hotmail.com>
2018-12-16 11:43:12 +0800

    修改了一下路由规则, 并新建接口方法, 获取到的参数

--- RouteDemo/Application/Home/Conf/config.php
+++ RouteDemo/Application/Home/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
     //'配置项'=>'配置值'
     'URL_ROUTE_RULES' => array(
-        'posts/:year/:month/:day' => 'Index/index',
+        'posts/:year/:month/:day' => 'Index/getRouteParam',
         'posts/:id' => 'Index/index',
         'posts/read/:id' => '/posts/:1'),
 );
